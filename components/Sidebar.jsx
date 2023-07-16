e33859fa4fed5bd4e5a99acbd33b972e35619cf5
sanketvagal <sanketdvagal@gmail.com>
2023-07-16 20:35:18 -0300

    Implemented Firestore database for storing notes

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -4,16 +4,15 @@ export default function Sidebar(props) {
     const noteElements = props.notes.map((note, index) => (
         <div key={note.id}>
             <div
-                
-                className={`title ${
-                    note.id === props.currentNote.id ? "selected-note" : ""
-                }`}
+
+                className={`title ${note.id === props.currentNote.id ? "selected-note" : ""
+                    }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
                 <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
-                <button 
+                <button
                     className="delete-btn"
-                    onClick={(event) => props.deleteNote(event, note.id)}
+                    onClick={() => props.deleteNote(note.id)}
                 >
                     <i className="gg-trash trash-icon"></i>
                 </button>
