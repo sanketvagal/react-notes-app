213eb112cad127bef3447e3b7a6a0522435e0463
sanketvagal <sanketdvagal@gmail.com>
2023-07-16 20:54:58 -0300

    Setting up env variables for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-	plugins: [react()]	
+	alias: {
+		'@': require('path').resolve(__dirname, 'src')
+	},
+	define: viteEnv,
+	plugins: [react()]
 })
+
+const viteEnv = {}
+Object.keys(process.env).forEach((key) => {
+	if (key.startsWith(`VITE_`)) {
+		viteEnv[`import.meta.env.${key}`] = process.env[key]
+	}
+})
+
